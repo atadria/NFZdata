b14d21cbadae45c18a34593bec097ad8a08cb989
Adrianna Tokarska <tokarska.adrianna@gmail.com>
2017-12-10 15:32:58 +0100

    Code refactor

--- website_parser.py
+++ website_parser.py
@@ -6,43 +6,51 @@ path = 'https://prog.nfz.gov.pl/app-jgp/KatalogJGP.aspx'
 driver = webdriver.Firefox()
 driver.get(path)
 
-options = []
-options_text_dict = {}
-select = Select(driver.find_element_by_name('ctl00$ContentPlaceHolder2$ddlSymylacjaJGP'))
-for option in select.options:
-    options.append(option.get_attribute('value'))
-    options_text_dict[option.get_attribute('value')] = option.text
 
-options = options[1:]
+def get_all_options(webdriver, contener):
+    """Get all options from menu, return dictionary with value and text"""
+    selected = Select(webdriver.find_element_by_name(contener))
+    options = []
+    options_text = []
+    for option in selected.options:
+        options.append(option.get_attribute('value'))
+        options_text.append(option.text)
+
+    return dict(zip(options[1:], options_text[1:]))
+
+
+# get years
+years_options = get_all_options(driver, 'ctl00$ContentPlaceHolder2$ddlSymylacjaJGP')
 
 all_links_from_year = {}
 
-# select years
-for option in options:
-    select = Select(driver.find_element_by_name('ctl00$ContentPlaceHolder2$ddlSymylacjaJGP'))
-    select.select_by_value(option)
-    options2 = []
-    all_links_from_year[options_text_dict[option]] = []
-    select2 = Select(driver.find_element_by_name('ctl00$ContentPlaceHolder2$ddlKatalogJGP'))
-    # select disease
-    for option2 in select2.options:
-        options2.append(option2.get_attribute('value'))
-
+for option in years_options.keys():
+    # select years
+    selected_year = Select(driver.find_element_by_name('ctl00$ContentPlaceHolder2$ddlSymylacjaJGP'))
+    selected_year.select_by_value(option)
+    # get diseases
+    diseases_options = get_all_options(driver, 'ctl00$ContentPlaceHolder2$ddlKatalogJGP')
+    # links from all years
+    all_links_from_year[years_options[option]] = []
+    # links from all diseases in year
     links_from_year = []
 
-    for option2 in options2:
-        select3 = Select(driver.find_element_by_name('ctl00$ContentPlaceHolder2$ddlKatalogJGP'))
-        select3.select_by_value(option2)
+    # select disease
+    for option2 in diseases_options.keys():
+        selected_disease = Select(driver.find_element_by_name('ctl00$ContentPlaceHolder2$ddlKatalogJGP'))
+        selected_disease.select_by_value(option2)
+        # links in disease
         elinks = []
         elinks = driver.find_elements_by_xpath("//a[@href]")
         links = []
         for elink in elinks:
             links.append(elink.get_attribute('href'))
         links_from_year += links[3:]
-    all_links_from_year[options_text_dict[option]] = links_from_year
+    all_links_from_year[years_options[option]] = links_from_year
 
-with open('links.txt', 'w') as f:
-    for item in all_links_from_year.keys():
-        f.write(item + "\n")
-        for urle in all_links_from_year[item]:
-            f.write(urle + "\n")
+# write to file
+with open('links.txt', 'w') as file:
+    for year in all_links_from_year.keys():
+        file.write(year + "\n")
+        for urle in all_links_from_year[year]:
+            file.write(urle + "\n")
